c201c72511feb57d9acd4b094cef21a2e8ca2a41
Milan Tarami <milantarami.dev@gmail.com>
2023-11-04 23:22:36 +0545

    chore: config

--- src/app/api/bookmarks/scrape/route.ts
+++ src/app/api/bookmarks/scrape/route.ts
@@ -1,36 +1,36 @@
-import axios from 'axios'
-import { URL } from 'url'
-import * as cheerio from 'cheerio'
+import axios from "axios";
+import { URL } from "url";
+import * as cheerio from "cheerio";
 
 export async function GET(request: Request) {
   try {
-    const url = new URL(request.url)
-    const params = new URLSearchParams(url.search)
-    const scrapeUrl = params.get('url') as string
-    const response = await axios.get(scrapeUrl)
-    const $ = cheerio.load(response.data)
+    const url = new URL(request.url);
+    const params = new URLSearchParams(url.search);
+    const scrapeUrl = params.get("url") as string;
+    const response = await axios.get(scrapeUrl);
+    const $ = cheerio.load(response.data);
 
-    const title: string = $('title').text() || 'No Title Found'
-    const metaDescription: string = $('meta[name="description"]').attr('content') || 'No Meta Description Found'
+    const title: string = $("title").text() || "No Title Found";
+    const metaDescription: string = $('meta[name="description"]').attr("content") || "No Meta Description Found";
 
-    const parsedURL: URL = new URL(scrapeUrl)
-    const baseURL: string = `${parsedURL.protocol}//${parsedURL.host}`
+    const parsedURL: URL = new URL(scrapeUrl);
+    const baseURL: string = `${parsedURL.protocol}//${parsedURL.host}`;
 
-    let faviconURL: string = ''
+    let faviconURL: string = "";
     $('link[rel="icon"]').each((index: number, element: cheerio.Element) => {
-      const href: string | undefined = $(element).attr('href')
+      const href: string | undefined = $(element).attr("href");
       if (href) {
-        faviconURL = new URL(href, baseURL).href
-        return false // Exit the loop on the first favicon found
+        faviconURL = new URL(href, baseURL).href;
+        return false; // Exit the loop on the first favicon found
       }
-    })
+    });
 
     if (!faviconURL) {
-      faviconURL = 'No Favicon Found'
+      faviconURL = "No Favicon Found";
     }
 
-    return Response.json({ title, metaDescription, baseURL, faviconURL })
+    return Response.json({ title, metaDescription, baseURL, faviconURL, scrapedURL: scrapeUrl });
   } catch (error) {
-    return Response.json({ error: 'error' })
+    return Response.json({ error: "error" });
   }
 }
